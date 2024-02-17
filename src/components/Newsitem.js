678d6202a18f3b14430098f8a7fab97993d19e02
Sudipkarmakar25 <153487702+Sudipkarmakar25@users.noreply.github.com>
2024-02-17 12:34:26 +0530

    my first commit

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -11,7 +11,7 @@ export class newsitem extends Component {
     <h5 className="card-title">{title}</h5>
     <p className="card-text">{description}</p>
     <p className="card-text"><small className="text-muted">By {!author?"Unknown":author} on {date}</small></p>
-    <a href={newsurl} target="_blank" className="btn btn-sm btn-primary">ReadMore</a>
+    <a href={newsurl} target="/" className="btn btn-sm btn-primary">ReadMore</a>
   </div>
 </div>
       </div>
